6580a045211b13265aa231c391fe53b1076c6f0f
Rohitb21 <rohit.bhardwaj9792@gmail.com>
2023-04-23 17:14:30 +0530

    from desktop

---
+++ assgn3.js
@@ -0,0 +1,12 @@
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name= "Rohit";
+let age = 25;
+console.log(name+age);
+console.log(typeof(name));
+console.log(typeof(age));
+
+
+
