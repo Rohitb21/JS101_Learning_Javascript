49fafa70dfab194073caa4e14fe8740a52dbe866
Rohitb21 <rohit.bhardwaj9792@gmail.com>
2023-04-23 17:14:59 +0530

    from desktop

---
+++ assgn4.js
@@ -0,0 +1,29 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+console.log("***************************************");
+console.log("");
+console.log("Ⓡ  Ⓔ  Ⓟ  Ⓞ  Ⓡ  Ⓣ      Ⓒ  Ⓐ  Ⓡ  Ⓓ");
+console.log("");
+
+console.log("ABC SCHOOL");
+console.log("Name: Piyush Bansal");
+console.log("Grade: 9th Section: D");
+console.log("Roll No.: 22");
+console.log("Maths: 50/60");
+console.log("Science: 55/60");
+console.log("English: 48/60");
+console.log("");
+
+console.log("***************************************");
+
+
+
+
+
+
+
+
+
+
+  
